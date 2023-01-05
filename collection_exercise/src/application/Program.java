4118f8651a79503430a179c1d5cb2dfc6a43b725
ibnergabriel <ibner68@gmail.com>
2023-01-05 09:07:34 -0300

    Criado as primeiras informações da classe  Program

--- collection_exercise/src/application/Program.java
+++ collection_exercise/src/application/Program.java
@@ -1,7 +1,28 @@
 package application;
 
+import entities.Client;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+
+        Scanner scanner = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter client data: ");
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Birth date: ");
+        String birthDate = scanner.nextLine();
+
+        Client client = new Client(name, email, sdf.parse(birthDate));
+
 
     }
 }
