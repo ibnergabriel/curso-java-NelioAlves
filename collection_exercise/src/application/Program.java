0390ede5ae008dd89521ae225098f6fc088fdbf4
ibnergabriel <ibner68@gmail.com>
2023-01-11 09:43:20 -0300

    Adições no programa principal

--- collection_exercise/src/application/Program.java
+++ collection_exercise/src/application/Program.java
@@ -2,17 +2,20 @@ package application;
 
 import entities.Client;
 import entities.Order;
+import entities.OrderItem;
 import entities.OrderStatus;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) throws ParseException {
 
         Scanner scanner = new Scanner(System.in);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
         System.out.println("Enter client data: ");
         System.out.print("Name: ");
@@ -22,16 +25,22 @@ public class Program {
         System.out.print("Birth date: ");
         String birthDate = scanner.nextLine();
 
-        Client client = new Client(name, email, sdf.parse(birthDate));
+        Client client = new Client(name, email, sdf1.parse(birthDate));
 
         System.out.println("Enter the order data: ");
         System.out.print("Status: ");
         String status = scanner.nextLine();
+
+        Order order = new Order( sdf2.parse(String.valueOf(new Date(System.currentTimeMillis()))),
+                OrderStatus.valueOf(status),
+                client);
+
+        System.out.println();
         System.out.print("How many items to this order? ");
         int quantityItems = scanner.nextInt();
 
-        Order order = new Order( ,OrderStatus.valueOf(status), client);
 
 
     }
+
 }
